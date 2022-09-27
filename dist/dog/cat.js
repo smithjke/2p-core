48ddc22c45908097287a7a0e1909a4934bc85441
smithjke <smithjke@gmail.com>
2022-09-27 18:07:53 +0300

    lol cat gus

---
+++ dist/dog/cat.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Cat = void 0;
+class Cat {
+    name = 'Tom';
+}
+exports.Cat = Cat;
---
+++ dist/dog/gus.d.ts
@@ -0,0 +1,1 @@
+export declare type Gus = string | null;
