86ccc117a2d2042105f012d7be940dcdd789626c
smithjke <smithjke@gmail.com>
2022-10-08 01:09:30 +0300

    fix: remove all.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
-import * as TPCore from './all';
+import * as api from './api';
+import * as crud from './crud';
+import * as di from './di';
+import * as jsonRpc from './json-rpc';
 
-export default TPCore;
+export default {
+  api,
+  crud,
+  di,
+  jsonRpc,
+};
